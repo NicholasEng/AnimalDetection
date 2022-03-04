ebb90e41e6f53f6a6c3efdf78f6d579c6bb1a142
Joel Lynn <joel@newton>
2022-03-03 18:05:59 -0700

    updated main to loop over videos

--- main.py
+++ main.py
@@ -2,114 +2,103 @@ import cv2
 import imutils
 import numpy as np
 import datetime
+from pathlib import Path
 
-videos = ["LNR408_ch2_main_20180726065707_20180726065726.dav", "LNR408_ch2_main_20180726063934_20180726064007.dav"]
+# An idea for a parameter in our future function is whether or not to
+# show a visual as the code runs.
+
+detected_images = {}
+detected_times = []
+videos = []
+video_dir = Path("./Videos")
+for video_path in video_dir.glob("*.dav"):
+    videos.append(str(video_path))
 videos.sort()
-current_video = "Videos/LNR408_ch0_main_20180726065707_20180726065726.dav.mp4"
-video_dt_format = "%Y%m%d%H%M%S"
-videoTimeStamp = datetime.datetime.strptime(current_video.split("_")[-2], video_dt_format)
 
+for video in videos:
+    video_dt_format = "%Y%m%d%H%M%S"
 
-# Video Capture 
-# capture = cv2.VideoCapture(0)
-#capture = cv2.VideoCapture("Videos/natureClip.mp4")
-#capture = cv2.VideoCapture("Videos/Caterpiller.mp4")
-capture = cv2.VideoCapture(current_video)
-#capture = cv2.VideoCapture("Videos/LNR408_ch2_main_20180726063934_20180726064007.dav")
-#capture = cv2.VideoCapture("Videos/LNR408_ch2_main_20180726065707_20180726065726.dav")
-# Offset of 5 seconds - subtract 5 seconds from filename
+    # Offset of 5 seconds - subtract 5 seconds from filename
+    videoTimeStamp = datetime.datetime.strptime(video.split("_")[-2], video_dt_format)
 
-#get frames per second
-fps = capture.get(cv2.CAP_PROP_FPS) #(cv2.CV_CAP_PROP_FPS
+    # Video Capture
+    capture = cv2.VideoCapture(video)
 
-# History, Threshold, DetectShadows 
-# fgbg = cv2.createBackgroundSubtractorMOG2(50, 200, True)
-fgbg = cv2.createBackgroundSubtractorMOG2(300, 100, True)
+    # get frames per second
+    fps = capture.get(cv2.CAP_PROP_FPS)  # (cv2.CV_CAP_PROP_FPS
 
-#Intialize Average
-avg = None
+    # History, Threshold, DetectShadows
+    fgbg = cv2.createBackgroundSubtractorMOG2(300, 100, True)
 
-# Keeps track of what frame we're on
-frameCount = 0
+    # Intialize Average
+    avg = None
 
-while(1):
-	# Return Value and the current frame
-	ret, frame = capture.read()
+    # Keeps track of what frame we're on
+    frameCount = 0
 
-	#  Check if a current frame actually exist
-	if not ret:
-		break
+    while 1:
+        # Return Value and the current frame
+        ret, frame = capture.read()
 
-	# Resize the frame
-	resizedFrame = cv2.resize(frame, (0, 0), fx=0.50, fy=0.50)
+        #  Check if a current frame actually exist
+        if not ret:
+            break
 
-	#blurs and converts image to grayscale
-	gray = cv2.cvtColor(resizedFrame, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        # Resize the frame
+        resizedFrame = cv2.resize(frame, (0, 0), fx=0.50, fy=0.50)
 
-	# if the average frame is None, initialize it
-	if avg is None:
-		avg = gray.copy().astype("float")
-		#capture.truncate(0)
-		continue
+        # blurs and converts image to grayscale
+        gray = cv2.cvtColor(resizedFrame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-	# accumulate the weighted average between the current frame and
-	# previous frames, then compute the difference between the current
-	# frame and running average
-	cv2.accumulateWeighted(gray, avg, 0.5)
-	frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+        # if the average frame is None, initialize it
+        if avg is None:
+            avg = gray.copy().astype("float")
+            continue
 
-	# threshold the delta image, dilate the thresholded image to fill
-	# in holes, then find contours on thresholded image
-	thresh = cv2.threshold(frameDelta, 7.5, 255,
-		cv2.THRESH_BINARY)[1] #5
-	thresh = cv2.dilate(thresh, None, iterations=2)
-	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
+        # accumulate the weighted average between the current frame and
+        # previous frames, then compute the difference between the current
+        # frame and running average
+        cv2.accumulateWeighted(gray, avg, 0.5)
+        frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
-	# loop over the contours
-	for c in cnts:
-		# if the contour is too small, ignore it
-		if cv2.contourArea(c) < 2500: #threshold number for contours
-			continue
-		# compute the bounding box for the contour, draw it on the frame,
-		# and update the text
-		(x, y, w, h) = cv2.boundingRect(c)
-		cv2.rectangle(resizedFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		text = "Animal"
-		cv2.putText(resizedFrame, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-		newTime = datetime.timedelta(0, time) + videoTimeStamp
-		print(newTime)
+        # threshold the delta image, dilate the thresholded image to fill
+        # in holes, then find contours on thresholded image
+        thresh = cv2.threshold(frameDelta, 7.5, 255, cv2.THRESH_BINARY)[1]  # 5
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
 
-	frameCount += 1
-	time = float(frameCount)/fps
-	newTime = datetime.timedelta(0, time) + videoTimeStamp
-	cv2.putText(resizedFrame, str(newTime), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        # loop over the contours
+        for c in cnts:
+            # if the contour is too small, ignore it
+            if cv2.contourArea(c) < 2500:  # threshold number for contours
+                continue
+            # compute the bounding box for the contour, draw it on the frame,
+            # and update the text
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(resizedFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            text = "Animal"
+            cv2.putText(resizedFrame, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            newTime = datetime.timedelta(0, time) + videoTimeStamp
+            print(newTime)
+            detected_times.append(newTime)
+            detected_images[newTime] = resizedFrame
 
-	# Get the foreground mask
-	#fgmask = fgbg.apply(resizedFrame)
+        frameCount += 1
+        time = float(frameCount) / fps
+        newTime = datetime.timedelta(0, time) + videoTimeStamp
+        cv2.putText(resizedFrame, str(newTime), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-	# Count all the non zero pixels within the mask
-	#count = np.count_nonzero(fgmask)
+        cv2.imshow("Frame", resizedFrame)
 
-	#print('Frame: %d, FPS: %d, Pixel Count: %d' % (frameCount, fps, count))
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
 
-	# Determine how many pixels do you want to detect to be considered "movement"
-	# if (frameCount > 1 and cou`nt > 5000):
+    capture.release()
+    cv2.destroyAllWindows()
 
-	# if (frameCount > 1 and count > 17000): #experiment with the thresh hold number
-	# 	print('Animal detected: %f' % time)
-	# 	cv2.putText(resizedFrame, 'Animal detected', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-	# 	#Output timestamp
-
-	cv2.imshow('Frame', resizedFrame)
-	# cv2.imshow('Mask', fgmask)
-
-
-	k = cv2.waitKey(1) & 0xff
-	if k == 27:
-		break
-
-capture.release()
-cv2.destroyAllWindows()
+with open("detected_times.txt", "w") as output_file:
+    for time in detected_times:
+        output_file.write(time.strftime(video_dt_format))
